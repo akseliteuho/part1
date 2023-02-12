0f58424f82247762d5406fbeaf093abd82a50e87
akseliteuho <ajteuh@utu.fi>
2023-02-12 20:57:40 +0200

    app1-1 valmis
    muokattu contentin kutsua

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Content = ({part1,part2,part3}) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part1={part1}/>
+      <Part part2/>
+      <Part part3/>
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
   )
 }
 
@@ -46,9 +48,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
+      <Content part={part1} exercises={exercises1} />
       <Total amount={exercises1 + exercises2 + exercises3}/>
       
     </div>
